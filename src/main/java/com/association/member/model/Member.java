87853839c9a8406e0f43bd825a57673e7c846c12
gmaminor <gbubemimaminor@gmail.com>
2023-10-28 13:19:25 +0100

    Member model created

--- src/main/java/com/association/member/model/Member.java
+++ src/main/java/com/association/member/model/Member.java
@@ -1,4 +1,20 @@
 package com.association.member.model;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Member {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private LocalDate dateOfBirth;
+
 }
